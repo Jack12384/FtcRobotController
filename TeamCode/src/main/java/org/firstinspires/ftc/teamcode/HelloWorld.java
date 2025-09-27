05def0f0225266c3891ad0ff120aec7fe43cadba
Jack12384 <jack.mays0612@gmail.com>
2025-09-27 16:26:37 -0500

    Create HelloWorld.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Disabled
+@Autonomous
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "Jack");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
